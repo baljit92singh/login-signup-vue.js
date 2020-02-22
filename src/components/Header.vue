d02676d61c95d1a04a66010e8c50196c15f489d3
Baljit Singh <baljit@bitcot.com>
2020-02-22 18:55:10 +0530

    added login, sign-up, reset-password

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <!-- <div id="nav"> -->
+    <md-toolbar class="md-primary" md-elevation="1">
+      <h3 class="md-title" style="flex: 1">Title</h3>
+      <md-button>Refresh</md-button>
+      <md-button class="md-primary">Create</md-button>
+    </md-toolbar>
+    <!-- <router-link to="/login">Login</router-link> |
+      <router-link to="/signup">Signup</router-link> -->
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
